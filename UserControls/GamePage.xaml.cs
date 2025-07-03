81bf9a4a55447a2d5cec296b98896744d96fb069
Bùi Duy Khánh <khanhbdhe186857@fpt.edu.vn>
2025-07-03 21:40:46 +0700

    add new game

--- UserControls/GamePage.xaml.cs
+++ UserControls/GamePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GameRentalManagement.Models;
 
 namespace GameRentalManagement
 {
@@ -20,9 +21,87 @@ namespace GameRentalManagement
     /// </summary>
     public partial class GamePage : UserControl
     {
+        GameRentalDbContext con = new GameRentalDbContext();
         public GamePage()
         {
             InitializeComponent();
+            LoadAllGames();
+        }
+        private void LoadAllGames()
+        {
+            var games = con.Games.ToList();
+            dgGameList.ItemsSource = games;
+
+        }
+        private void FilterGames(string keywords)
+        {
+            var games = con.Games
+                .Where(g => (g.Platform != null && g.Platform.ToLower().Contains(keywords)) ||
+                            (g.GameName != null && g.GameName.ToLower().Contains(keywords)) ||
+                            (g.PricePerDay != null && g.PricePerDay.ToString().Contains(keywords)) ||
+                            (g.Genre != null && g.Genre.ToLower().Contains(keywords))).ToList();
+            dgGameList.ItemsSource = games;
+        }
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string keywords = txtFilter.Text.ToLower();
+            FilterGames(keywords);
+        }
+        
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(con.Games.Any(g => g.GameName == txtGameName.Text))
+                {
+                    MessageBox.Show("The game with the same name existed." ,"Duplicate Name of Game", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+                if(string.IsNullOrWhiteSpace(txtGameName.Text) || 
+                   string.IsNullOrWhiteSpace(txtPlatform.Text) ||
+                   string.IsNullOrWhiteSpace(txtGenre.Text) ||
+                   string.IsNullOrWhiteSpace(txtQuantity.Text) ||
+                   string.IsNullOrWhiteSpace(txtPrice.Text))
+                {;
+                    MessageBox.Show("Please fill all of fields above, these fields must not be empty", "Input Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+                Game newGame = new Game
+                {
+                    GameName = txtGameName.Text,
+                    Platform = txtPlatform.Text,
+                    Genre = txtGenre.Text,
+                    Quantity = int.Parse(txtQuantity.Text),
+                    PricePerDay = decimal.Parse(txtPrice.Text),
+                    Status = false,
+                };
+                con.Games.Add(newGame);
+                con.SaveChanges();
+                MessageBox.Show("New game added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                LoadAllGames();
+
+            }
+            catch (Exception)
+            {
+
+               
+            }
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
